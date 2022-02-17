de929d3613ca93cb69d5f0c71e4b0588b9fd3db8
a-somebody <2199293791@qq.com>
2022-02-17 12:11:49 +0800

    Initial commit

---
+++ tupian3.py
@@ -0,0 +1,37 @@
+
+
+from __future__ import print_function, division
+
+import turtle
+
+
+def koch(t, n):
+    """画一个长为n的koch曲线。"""
+    if n < 10:
+        t.fd(n)
+        return
+    m = n/3
+    koch(t, m)
+    t.lt(60)
+    koch(t, m)
+    t.rt(120)
+    koch(t, m)
+    t.lt(60)
+    koch(t, m)
+
+
+def snowflake(t, n):
+    """画一个雪花 (每条边都有koch曲线的三角形)."""
+    for i in range(3):
+        koch(t, n)
+        t.rt(120)
+
+
+bob = turtle.Turtle()
+
+bob.pu()
+bob.goto(-150, 90)
+bob.pd()
+snowflake(bob, 300)
+
+turtle.mainloop()
